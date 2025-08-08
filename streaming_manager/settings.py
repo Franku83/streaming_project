0748caef59fec18d7ef88eb93bd9a2eb43ac60c6
Franku83 <Franku8383@gmail.com>
2025-08-07 23:52:15 -0400

    Configurado ALLOWED_HOSTS para Railway

--- streaming_manager/settings.py
+++ streaming_manager/settings.py
@@ -123,3 +123,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
