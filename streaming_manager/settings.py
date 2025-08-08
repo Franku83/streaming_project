9823499ae3c79ebea9fd791809c20118bda8aaec
Franku83 <Franku8383@gmail.com>
2025-08-07 23:48:52 -0400

    Añadido SECURE_PROXY_SSL_HEADER para Railway

--- streaming_manager/settings.py
+++ streaming_manager/settings.py
@@ -122,3 +122,4 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
